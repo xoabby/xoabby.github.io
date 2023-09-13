cca3fab226eab32c1770f51c9d890b7b13788f37
abby ⠀ <abigail.lunaduarte@sciacademy.org>
2023-09-13 20:49:30 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,9 +43,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(40, 170, 200, 20)
+    createPlatform(40, 170, 130, 20)
     createPlatform(350,250,200,20)
     createPlatform(550,350,200,20)
+    createPlatform(1050,500,100,150)
+    createPlatform(100,30,50,30)
     
 
 
@@ -55,7 +57,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable('diamond',550,350,0.5,0.1)
+    createCollectable('diamond',550,350,0,0)
 
 
 
